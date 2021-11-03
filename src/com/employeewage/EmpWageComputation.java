1fe9bb3d5a540a7744d745a53fb576935259b9c4
ManikantaGopisetti <manikantagopisetti2@gmail.com>
2021-11-03 09:18:03 +0530

    added welcome message

---
+++ src/com/employeewage/EmpWageComputation.java
@@ -0,0 +1,12 @@
+package com.employeewage;
+
+public class EmpWageComputation {
+
+	public static void main(String[] args) {
+
+		// Start with Displaying Welcome to Employee Wage Computation Program on Master
+		// Branch
+		System.out.println("Welcome to Employee Wage Computation Program");
+	}
+
+}
